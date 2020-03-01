02567941bae73236ab7e88e012d3b494cc035d66
SlobodanBozic <slobodan.bozic3@gmail.com>
2020-03-01 09:54:47 +0100

    first commit

---
+++ config/database.php
@@ -0,0 +1,27 @@
+<?php
+class Database{
+
+    // specify your own database credentials
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $db_name = "repaircar-db";
+    public $conn;
+
+    // get the database connection
+    public function getConnection(){
+
+            $this->conn = null;
+
+            $this->conn =  new mysqli($this->host, $this->username, $this->password, $this->db_name);
+            $this->conn->set_charset("utf8mb4");
+
+            // Check database connection
+            if ($this->conn === false){
+              echo "Failed to connect to MySQL: " . $this->conn->connect_error;
+              }
+
+        return $this->conn;
+    }
+}
+?>
